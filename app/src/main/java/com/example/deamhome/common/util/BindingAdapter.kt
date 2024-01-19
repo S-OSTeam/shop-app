679f8537099610fa700efeac5777cb17285de55d
mendel <rhthrhrl0@naver.com>
2024-01-19 15:52:31 +0900

    feat: BindingAdapter 유틸 추가

---
+++ app/src/main/java/com/example/deamhome/common/util/BindingAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.deamhome.common.util
+
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.view.isVisible
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.google.android.material.chip.Chip
+
+@BindingAdapter("imageUrl", "placeholder", requireAll = false)
+fun ImageView.setImageUrl(url: String?, placeholder: Drawable? = null) {
+    Glide.with(context)
+        .load(url)
+        .placeholder(placeholder)
+        .error(placeholder)
+        .into(this)
+}
+
+@BindingAdapter("imageUri", "placeholder", requireAll = false)
+fun ImageView.setImageUrl(uri: Uri?, placeholder: Drawable? = null) {
+    uri?.let {
+        Glide.with(context)
+            .load(it)
+            .error(placeholder)
+            .into(this)
+    }
+}
+
+@BindingAdapter("isVisible")
+fun View.isVisible(isVisible: Boolean) {
+    this.isVisible = isVisible
+}
+
+@BindingAdapter("onCloseClick")
+fun Chip.onCloseClick(onCloseClick: () -> Unit) {
+    this.setOnCloseIconClickListener {
+        onCloseClick()
+    }
+}
+
+@BindingAdapter("setTextOrEmpty")
+fun TextView.setTextOrEmpty(text: String?) {
+    this.text = text ?: ""
+}
+
+@BindingAdapter("isSelected")
+fun View.isSelected(isSelected: Boolean) {
+    this.isSelected = isSelected
+}
